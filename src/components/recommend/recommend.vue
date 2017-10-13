6c5e44615c6c5feb63d05f57f45cf78233153626
yangkaik <yangkaik_1108@qq.com>
2017-10-13 21:58:37 +0800

    图片懒加载（vue-lazyload）完成 loading层 完成

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -6,7 +6,8 @@
           <slider>
             <div v-for="item in recommends">
               <a :href="item.linkUrl">
-                <img @load="loadImage" :src="item.picUrl" alt="">
+                <!-- 插件fastclick自带方法，检查有无needsclick -->
+                <img class="needsclick" @load="loadImage" :src="item.picUrl" alt="">
               </a>
             </div>
           </slider>
@@ -16,7 +17,7 @@
           <ul>
             <li @click="selectItem(item)" v-for="item in discList" class="item">
               <div class="icon">
-                <img width="60" height="60" :src="item.imgurl">
+                <img width="60" height="60" v-lazy="item.imgurl">
               </div>
               <div class="text">
                 <h2 class="name" v-html="item.creator.name"></h2>
@@ -26,11 +27,15 @@
           </ul>
         </div>
       </div>
+      <div class="loading-container">
+        <loading v-show="!discList.length"></loading>
+      </div>
     </scroll>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import Loading from 'base/loading/loading'
   import Scroll from 'base/scroll/scroll'
   import { getRecommend, getDiscList } from 'api/recommend'
   import { ERR_OK } from 'api/config'
@@ -68,7 +73,6 @@
       loadImage () {
         // 技巧，定义参数，避免多次加载后重复执行refresh方法
         if (!this.checkloaded) {
-          console.log(!this.checkloaded)
           this.$refs.scroll.refresh()
           this.checkloaded = true
         }
@@ -76,7 +80,8 @@
     },
     components: {
       Slider,
-      Scroll
+      Scroll,
+      Loading
     }
   }
 </script>
--- src/main.js
+++ src/main.js
@@ -4,9 +4,15 @@ import App from './App'
 import fastclick from 'fastclick'
 import router from './router'
 import 'common/stylus/index.styl'
+import VueLeazyLoad from 'vue-lazyload'
 
 // 解决移动端点击300毫秒延迟问题
 fastclick.attach(document.body)
+
+Vue.use(VueLeazyLoad, {
+  // require语法是webpack支持的，将图片替换成base64格式
+  loading: require('common/image/default.png')
+})
 
 /* eslint-disable no-new */
 new Vue({
