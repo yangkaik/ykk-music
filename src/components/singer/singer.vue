58a56d21095064e99018e1b51ffde6fb60332277
yangkaik <yangkaik_1108@qq.com>
2017-11-23 22:52:50 +0800

    获取歌手数据，排序数据

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,12 +1,92 @@
 <template>
-  <div>
+  <div class="singer">
     singer
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  export default {}
+  import { getSingerList } from 'api/singer'
+  import { ERR_OK } from 'api/config'
+  import Singer from 'common/js/singer'
+
+  const HOT_NAME = '热门'
+  const HOT_SINGER_LEN = 10
+  export default {
+    // data是一个function
+    data () {
+      return {
+        // Object
+        singers: []
+      }
+    },
+    created () {
+      this._getSingerList()
+    },
+    methods: {
+      _getSingerList () {
+        getSingerList().then((res) => {
+          if (res.code === ERR_OK) {
+            this.singers = this._normalizeSinger(res.data.list)
+          }
+        })
+      },
+      // 排序数据
+      _normalizeSinger (list) {
+        let map = {
+          hot: {
+            title: HOT_NAME,
+            items: []
+          }
+        }
+        list.forEach((item, index) => {
+          if (index < HOT_SINGER_LEN) {
+            map.hot.items.push(
+              new Singer({
+                id: item.Fsinger_mid,
+                name: item.Fsinger_name
+              })
+            )
+          }
+          const key = item.Findex
+          if (!map[key]) {
+            map[key] = {
+              title: key,
+              items: []
+            }
+          }
+          map[key].items.push(
+            new Singer({
+              id: item.Fsinger_mid,
+              name: item.Fsinger_name
+            })
+          )
+        })
+        // 为了得到有序列表，我们需要处理 map
+        let hot = []
+        let ret = []
+        for (let key in map) {
+          let val = map[key]
+          if (val.title.match(/[a-zA-Z]/)) {
+            ret.push(val)
+          } else if (val.title === HOT_NAME) {
+            hot.push(val)
+          }
+        }
+        ret.sort((a, b) => {
+          // charCodeAt() 方法可返回指定位置的字符的 Unicode 编码
+          return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+        })
+        // concat() 方法用于连接两个或多个数组。
+        return hot.concat(ret)
+      }
+    }
+  }
 </script>
 
-<style scoped lang="stylus" >
+<style scoped lang="stylus">
+  .singer
+    position: fixed
+    top: 88px
+    bottom: 0
+    width: 100%
 </style>
