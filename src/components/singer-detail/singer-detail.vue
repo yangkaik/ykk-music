5b5bd62de96063b877a2f9471867913a014ede51
yangkaik <yangkaik_1108@qq.com>
2017-11-29 22:58:24 +0800

    歌曲列表

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,39 +1,46 @@
 <template>
   <transition name="slide">
-    <div class="singer-detall">
-    </div>
+    <music-list :title="title" :bg-image="bgImage" :songs="songs"></music-list>
   </transition>
 </template>
 
 <script type="text/ecmascript-6">
   import { mapGetters } from 'vuex'
   import { getSingerDetail } from 'api/singer'
+  import { ERR_OK } from 'api/config'
   import { createSong } from 'common/js/song'
+  import MusicList from 'components/music-list/music-list'
 
   export default {
     data () {
       return {
-        song: []
+        songs: []
       }
     },
     computed: {
+      title () {
+        return this.singer.name
+      },
+      bgImage () {
+        return this.singer.avatar
+      },
       ...mapGetters([
         'singer'
       ])
     },
     created () {
       this._getDetail()
-      console.log(this.singer)
     },
     methods: {
       _getDetail () {
-        // 没有取到id，返回singer页面
         if (!this.singer.id) {
           this.$router.push('/singer')
+          return
         }
         getSingerDetail(this.singer.id).then((res) => {
-          this.song = this._normalizeSongs(res.data.list)
-          console.log(this.song)
+          if (res.code === ERR_OK) {
+            this.songs = this._normalizeSongs(res.data.list)
+          }
         })
       },
       _normalizeSongs (list) {
@@ -46,21 +53,15 @@
         })
         return ret
       }
+    },
+    components: {
+      MusicList
     }
   }
 </script>
 
 <style scoped lang="stylus">
   @import "~common/stylus/variable"
-  .singer-detall
-    position: fixed
-    z-index: 100
-    top: 0
-    left: 0
-    right: 0
-    bottom: 0
-    background: $color-background
-
   .slide-enter-active, .slide-leave-active
     transition: all 0.3s
 
